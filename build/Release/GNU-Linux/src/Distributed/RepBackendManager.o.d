7a19be4f69addc20e4ce35ddbce2a3886ea32f38
Le Hai Son <lehaisonmath6@gmail.com>
2019-06-29 16:34:50 +0700

    update etcdregister

---
+++ build/Release/GNU-Linux/src/Distributed/RepBackendManager.o.d
@@ -0,0 +1,98 @@
+build/Release/GNU-Linux/src/Distributed/RepBackendManager.o: \
+ src/Distributed/RepBackendManager.cpp \
+ inc/Distributed/RepBackendManager.h inc/Distributed/BackendManager.h \
+ inc/Distributed/BackendInfo.h inc/Hashing/Hasher.h \
+ inc/Hashing/DefaultHasher.h ../../contribs/Poco/include/Poco/SharedPtr.h \
+ ../../contribs/Poco/include/Poco/Foundation.h \
+ ../../contribs/Poco/include/Poco/Config.h \
+ ../../contribs/Poco/include/Poco/Platform.h \
+ ../../contribs/Poco/include/Poco/Platform_POSIX.h \
+ ../../contribs/Poco/include/Poco/Alignment.h \
+ ../../contribs/Poco/include/Poco/Bugcheck.h \
+ ../../contribs/Poco/include/Poco/Types.h \
+ ../../contribs/Poco/include/Poco/Exception.h \
+ ../../contribs/Poco/include/Poco/AtomicCounter.h \
+ ../../contribs/Poco/include/Poco/Mutex.h \
+ ../../contribs/Poco/include/Poco/ScopedLock.h \
+ ../../contribs/Poco/include/Poco/Mutex_POSIX.h \
+ ../../contribs/Poco/include/Poco/Event.h \
+ ../../contribs/Poco/include/Poco/Event_POSIX.h \
+ ../../contribs/Poco/include/Poco/ActiveMethod.h \
+ ../../contribs/Poco/include/Poco/ActiveResult.h \
+ ../../contribs/Poco/include/Poco/RefCountedObject.h \
+ ../../contribs/Poco/include/Poco/ActiveRunnable.h \
+ ../../contribs/Poco/include/Poco/Runnable.h \
+ ../../contribs/Poco/include/Poco/AutoPtr.h \
+ ../../contribs/Poco/include/Poco/ActiveStarter.h \
+ ../../contribs/Poco/include/Poco/ThreadPool.h \
+ ../../contribs/Poco/include/Poco/Thread.h \
+ ../../contribs/Poco/include/Poco/Thread_POSIX.h \
+ ../../contribs/Poco/include/Poco/SignalHandler.h \
+ ../../contribs/Poco/include/Poco/RWLock.h \
+ ../../contribs/Poco/include/Poco/RWLock_POSIX.h
+
+inc/Distributed/RepBackendManager.h:
+
+inc/Distributed/BackendManager.h:
+
+inc/Distributed/BackendInfo.h:
+
+inc/Hashing/Hasher.h:
+
+inc/Hashing/DefaultHasher.h:
+
+../../contribs/Poco/include/Poco/SharedPtr.h:
+
+../../contribs/Poco/include/Poco/Foundation.h:
+
+../../contribs/Poco/include/Poco/Config.h:
+
+../../contribs/Poco/include/Poco/Platform.h:
+
+../../contribs/Poco/include/Poco/Platform_POSIX.h:
+
+../../contribs/Poco/include/Poco/Alignment.h:
+
+../../contribs/Poco/include/Poco/Bugcheck.h:
+
+../../contribs/Poco/include/Poco/Types.h:
+
+../../contribs/Poco/include/Poco/Exception.h:
+
+../../contribs/Poco/include/Poco/AtomicCounter.h:
+
+../../contribs/Poco/include/Poco/Mutex.h:
+
+../../contribs/Poco/include/Poco/ScopedLock.h:
+
+../../contribs/Poco/include/Poco/Mutex_POSIX.h:
+
+../../contribs/Poco/include/Poco/Event.h:
+
+../../contribs/Poco/include/Poco/Event_POSIX.h:
+
+../../contribs/Poco/include/Poco/ActiveMethod.h:
+
+../../contribs/Poco/include/Poco/ActiveResult.h:
+
+../../contribs/Poco/include/Poco/RefCountedObject.h:
+
+../../contribs/Poco/include/Poco/ActiveRunnable.h:
+
+../../contribs/Poco/include/Poco/Runnable.h:
+
+../../contribs/Poco/include/Poco/AutoPtr.h:
+
+../../contribs/Poco/include/Poco/ActiveStarter.h:
+
+../../contribs/Poco/include/Poco/ThreadPool.h:
+
+../../contribs/Poco/include/Poco/Thread.h:
+
+../../contribs/Poco/include/Poco/Thread_POSIX.h:
+
+../../contribs/Poco/include/Poco/SignalHandler.h:
+
+../../contribs/Poco/include/Poco/RWLock.h:
+
+../../contribs/Poco/include/Poco/RWLock_POSIX.h:
